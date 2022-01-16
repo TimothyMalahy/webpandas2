c46c05f48efdcbd785b23dde32390a642e3c0342
Timothy Malahy <TimothyMalahy@gmail.com>
2022-01-16 12:05:32 -0600

    commented out contrib user

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from .validators import validate_file_extension, valid_extensions
 from django.db.models.signals import post_save
 from django.dispatch import receiver
